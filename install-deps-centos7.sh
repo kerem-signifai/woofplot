fe2aadbb178584f6044dd681417468cca943f59c
root <root@euca-10-1-5-108.eucalyptus.internal>
2020-10-23 19:19:34 +0000

    working on centos 7 install

--- install-deps-centos7.sh
+++ install-deps-centos7.sh
@@ -3,10 +3,15 @@
 
 yum -y update
 yum -y install java-1.8.0-openjdk-devel
+PTEST=`grep JAVA_HOME /root/.bashrc`
+if [[ ! -z "$PTEST" ]] ; then
+	echo "JAVA_HOME already set to " $PTEST
+	exit 1
+fi
 echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0" >> ~/.bashrc
 curl https://bintray.com/sbt/rpm/rpm | tee /etc/yum.repos.d/bintray-sbt-rpm.repo
 yum -y install sbt
-curl --silent --location https://rpm.nodesource.com/setup_10.x
+curl --silent --location https://rpm.nodesource.com/setup_10.x | bash -
 yum -y install nodejs
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
 rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
@@ -25,3 +30,5 @@ sslcacert=/etc/pki/tls/certs/ca-bundle.crt
 metadata_expire=300
 EOL
 yum update -y
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose

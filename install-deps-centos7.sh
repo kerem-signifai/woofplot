f2b9a96d0f00ae84631a76722505b490573bb7b9
root <root@euca-10-1-5-108.eucalyptus.internal>
2020-10-23 18:00:30 +0000

    adding install script

---
+++ install-deps-centos7.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# run this as root
+
+yum -y update
+yum -y install java-1.8.0-openjdk
+echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.191.b12-1.el7_6.x86_64/jre/bin/java" >> ~/.bashrc
+curl https://bintray.com/sbt/rpm/rpm | tee /etc/yum.repos.d/bintray-sbt-rpm.repo
+yum -y install sbt
+curl --silent --location https://rpm.nodesource.com/setup_10.x
+yum -y install nodejs
+curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
+rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
+yum -y install yarn
+yum install -y https://download.postgresql.org/pub/repos/yum/11/redhat/rhel-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+tee /etc/yum.repos.d/timescale_timescaledb.repo <<EOL
+[timescale_timescaledb]
+name=timescale_timescaledb
+baseurl=https://packagecloud.io/timescale/timescaledb/el/7/\$basearch
+repo_gpgcheck=1
+gpgcheck=0
+enabled=1
+gpgkey=https://packagecloud.io/timescale/timescaledb/gpgkey
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+metadata_expire=300
+EOL
+yum update -y
